b84f879ec7a8b5212c0ed9129f4529f028dc537b
Allie Robinson <allierob93@gmail.com>
2023-07-05 17:08:18 -0500

    :art: format

--- src/Pages/BasicCare.jsx
+++ src/Pages/BasicCare.jsx
@@ -19,43 +19,48 @@ export const BasicCare = () => {
       <Typography variant="h1" sx={{ mb: 2 }}>
         Budgie Fact Sheet
       </Typography>
-      <Typography variant="h6">Cage Specifications</Typography>
-      <Typography variant="body1">
-        <ul>
-          <li>
-            <strong>Minimum cage size:</strong> 30&quot; x 18&quot; x 18&quot;
-          </li>
-          <li>
-            <strong>Maximum bar gap:</strong> 1/2&quot;
-          </li>
-          <li>
-            <strong>Horizontal bars preferred?</strong> Yes
-          </li>
-          <li>
-            <strong>Corners preferred?</strong> Yes
-          </li>
-          <li>
-            <strong>Wooden cage?</strong> No
-          </li>
-        </ul>
-      </Typography>
-      <Typography variant="h6">Diet</Typography>
-      <Typography variant="body1">
-        <ul>
-          <li>
-            <strong>Pellets:</strong> Unlimited access
-          </li>
-          <li>
-            <strong>Veggies:</strong> Daily
-          </li>
-          <li>
-            <strong>Seeds:</strong> 1.5-2 level teaspoons per bird daily
-          </li>
-          <li>
-            <strong>Supplements:</strong> milk thistle, probiotics
-          </li>
-        </ul>
-      </Typography>
+      <Box sx={{ width: "500px", maxWidth: "100%" }}>
+        <Typography variant="h6">Cage Specifications</Typography>
+        <Typography variant="body1">
+          <ul>
+            <li>
+              <strong>Minimum cage size:</strong> 30&quot; x 18&quot; x 18&quot;
+            </li>
+            <li>
+              <strong>Maximum bar gap:</strong> 1/2&quot;
+            </li>
+            <li>
+              <strong>Horizontal bars preferred?</strong> Yes
+            </li>
+            <li>
+              <strong>Corners preferred?</strong> Yes
+            </li>
+            <li>
+              <strong>Wooden cage?</strong> No
+            </li>
+          </ul>
+        </Typography>
+        <Typography variant="h6">Diet</Typography>
+        <Typography variant="body1">
+          <ul>
+            <li>
+              <strong>Pellets:</strong> Unlimited access
+            </li>
+            <li>
+              <strong>Veggies:</strong> Daily
+            </li>
+            <li>
+              <strong>Seeds:</strong> 1.5-2 level teaspoons per bird daily
+            </li>
+            <li>
+              <strong>Water:</strong> Refresh daily
+            </li>
+            <li>
+              <strong>Supplements:</strong> Milk thistle, probiotics
+            </li>
+          </ul>
+        </Typography>
+      </Box>
     </Box>
   );
 };
