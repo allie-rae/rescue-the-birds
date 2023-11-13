355f92e6dc3d34e5e9237ec56e5306a28d613e06
Jason Zubiate <jason@Jasons-Mac-mini.local>
2023-11-13 09:09:51 -0800

    adds amazon wish list

--- src/Pages/Donate.jsx
+++ src/Pages/Donate.jsx
@@ -26,10 +26,25 @@ export const Donate = () => {
           Donate
         </Typography>
         <Box sx={{ width: "600px", maxWidth: "100%" }}>
-          Thank you so much for your interest in donating. Each animal in our shelter is given the
-          very best avian veterinary care whenver it&apos;s needed, a healthy diet, a clean, safe
-          enclosure, regular beak and nail filings, and plenty of enrichment so they can live
-          happily while they&apos;re with us at the Refuge.
+          <Typography variant="body1" sx={{ mb: 4 }}>
+            Thank you so much for your interest in donating. Each animal in our shelter is given the
+            very best avian veterinary care whenver it&apos;s needed, a healthy diet, a clean, safe
+            enclosure, regular beak and nail filings, and plenty of enrichment so they can live
+            happily while they&apos;re with us at the Refuge.
+          </Typography>
+          <Typography variant="h2" sx={{ mb: 2, mt: 2 }}>
+            Spread joy to our feathered friends
+          </Typography>
+          <Typography variant="body1" sx={{ mb: 4, mt: 2 }}>
+            Explore our{" "}
+            <a href="https://www.amazon.com/hz/wishlist/ls/1YMMJ3VB5032X/ref=hz_ls_biz_ex">
+              Amazon Wish List
+            </a>{" "}
+            and directly contribute to the well-being of our avian companions. From treats to
+            shelter essentials, your generosity ensures a life of comfort, health, and happiness.
+            Join us in creating a sanctuary where every wing flap echoes with gratitude. Be a part
+            of transforming wishes into reality. Thank you for making a difference!
+          </Typography>
           <br />
           <br />
           {/* TODO: check with Rich re: how much it costs to care for one bird/month. 
