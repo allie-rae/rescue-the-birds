228a0da6da9f0c36f32656351fa34f614947ef0c
Johnathon Bowers <johnathonbowers@gmail.com>
2024-01-21 15:05:34 -0500

    Add hold harmless agreement text
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/Pages/Volunteer.jsx
+++ src/Pages/Volunteer.jsx
@@ -453,6 +453,21 @@ export const Volunteer = () => {
                   writing.
                 </FormHelperText>
               </FormGroup>
+              <Typography
+                variant="body1"
+                sx={{ fontWeight: "bold" }}
+              >
+                Volunteer Hold Harmless Agreement:
+              </Typography>
+              <FormHelperText>
+                Agreement made this {currentDateAsOrdinal} day of {currentMonth}, {currentYear} by and between A Refuge for Saving the Wildlife, Incorporated&copy; (hereinafter referred to as RSW&copy;, Inc.), located in Northbrook, IL, and (hereinafter referred to as Volunteer).
+              </FormHelperText>
+              <FormHelperText>
+                Whereas RSW&copy;, Inc. desires to accept the gratuitous services of the Volunteer, and the Volunteer desires to provide gratuitous services to RSW&copy;, Inc.
+              </FormHelperText>
+              <FormHelperText>
+                In consideration of the following premises and mutual covenants, and other good and valuable considerations, the receipt and sufficiency of which are hereby acknowledged, it is mutually covenanted and agreed by and between the parties hereto as follows:
+              </FormHelperText>
               {/* Hidden input used to validate whether or not a user has selected an area of interest for volunteering. This input is registered to React Hook Form in order to make use of its validation functionality */}
               <input
                 type="hidden"
