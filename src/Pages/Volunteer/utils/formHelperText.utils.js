6573edf286a68d33c0726979b49da16aa2891e82
Johnathon Bowers <johnathonbowers@gmail.com>
2024-01-25 14:58:46 -0500

    Add utility function to generate dynamic Hold Harmless preamble
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

---
+++ src/Pages/Volunteer/utils/formHelperText.utils.js
@@ -0,0 +1,28 @@
+import { DateUtilities } from "../../../utils/date.utils.js";
+import { FormHelperText } from "@mui/material";
+
+const getHoldHarmlessPreamble = (personName) => {
+
+    const currentDateAsOrdinal = DateUtilities.getCurrentDateAsOrdinal();
+    const currentMonth = DateUtilities.getCurrentMonth();
+    const currentYear = DateUtilities.getCurrentFullYear();
+    
+    return (
+        <>
+            <FormHelperText>
+                Agreement made this {currentDateAsOrdinal} day of {currentMonth}, {currentYear} by and between A Refuge for Saving the Wildlife, Incorporated&copy; (hereinafter referred to as RSW&copy;), located in Northbrook, IL, and {personName} (hereinafter referred to as Volunteer).
+            </FormHelperText>
+            <FormHelperText>
+                Whereas RSW&copy; desires to accept the gratuitous services of the Volunteer, and the Volunteer desires to provide gratuitous services to RSW&copy;.
+            </FormHelperText>
+            <FormHelperText>
+                In consideration of the following premises and mutual covenants, and other good and valuable considerations, the receipt and sufficiency of which are hereby acknowledged, it is mutually covenanted and agreed by and between the parties hereto as follows:
+            </FormHelperText>
+        </>
+    );
+
+}
+
+export const formHelperTextUtilities = {
+    getHoldHarmlessPreamble,
+}
