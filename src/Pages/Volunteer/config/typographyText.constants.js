76c2602be7512e63015b29c3d1c5bdd5935033d9
Johnathon Bowers <johnathonbowers@gmail.com>
2024-02-01 16:41:40 -0500

    Add formIntroduction and formInstructions to typographyTextContent
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

---
+++ src/Pages/Volunteer/config/typographyText.constants.js
@@ -0,0 +1,7 @@
+export const typographyTextContent = {
+
+    formIntroduction: `Thank you for your interest in volunteering at A Refuge for Saving the Wildlife. We rely on volunteers to support our mission and operations every day. You can be a part of our life-saving work. By choosing to volunteer your time and talents, you are helping to make a difference to all the parrots housed here. Whether caring for parrots, managing operations, or cleaning, volunteers are the key to our success. There are many ways you can help out at A Refuge for Saving the Wildlife. Continue reading below to find out how!`,
+
+    formInstructions: `To apply, please fill out the form below. Once you complete the volunteer application, your information will be reviewed and you will be contacted by our Volunteer Coordinator.`,
+
+}
