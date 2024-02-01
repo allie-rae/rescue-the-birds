ab82475ea7bbb2b68118fcbf03c88a623991c34b
Johnathon Bowers <johnathonbowers@gmail.com>
2024-02-01 16:40:52 -0500

    Change components from FormHelperText to Typography
    Signed-off-by: Johnathon Bowers <johnathonbowers@gmail.com>

--- src/Pages/Volunteer/utils/formHelperText.utils.js
+++ src/Pages/Volunteer/utils/formHelperText.utils.js
@@ -1,5 +1,5 @@
 import { DateUtilities } from "../../../utils/index.js";
-import { FormHelperText } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const getHoldHarmlessPreamble = (personName) => {
 
@@ -9,15 +9,24 @@ const getHoldHarmlessPreamble = (personName) => {
     
     return (
         <>
-            <FormHelperText>
+            <Typography
+            variant="body1"
+            sx={{ mb: 2 }}
+            >
                 Agreement made this {currentDateAsOrdinal} day of {currentMonth}, {currentYear} by and between A Refuge for Saving the Wildlife, Incorporated&copy; (hereinafter referred to as RSW&copy;), located in Northbrook, IL, and {personName} (hereinafter referred to as Volunteer).
-            </FormHelperText>
-            <FormHelperText>
+            </Typography>
+            <Typography
+            variant="body1"
+            sx={{ mb: 2 }}
+            >
                 Whereas RSW&copy; desires to accept the gratuitous services of the Volunteer, and the Volunteer desires to provide gratuitous services to RSW&copy;.
-            </FormHelperText>
-            <FormHelperText>
+            </Typography>
+            <Typography
+            variant="body1"
+            sx={{ mb: 2 }}
+            >
                 In consideration of the following premises and mutual covenants, and other good and valuable considerations, the receipt and sufficiency of which are hereby acknowledged, it is mutually covenanted and agreed by and between the parties hereto as follows:
-            </FormHelperText>
+            </Typography>
         </>
     );
 
